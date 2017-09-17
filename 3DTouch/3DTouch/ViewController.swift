6b7ad98649f3dcc4b074a7aa27f959218240f4d5
Joyce <469087843@qq.com>
2017-09-17 13:37:17 +0800

    nothing

--- 3DTouch/3DTouch/ViewController.swift
+++ 3DTouch/3DTouch/ViewController.swift
@@ -13,15 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let label = UILabel();
         
-        label.text = "Joyce"
-    
-        label.frame = CGRect.init(x: 100, y: 100, width: 100, height: 100)
+        self.view,.bac
         
-        label.backgroundColor = UIColor.red
-        
-        self.view.addSubview(label)
     
     }
 
